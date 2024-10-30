5c4c2fdeea36d316bc2620d35cf7caa302996b91
Paul Leveau <plawn.yay@gmail.com>
2024-10-30 11:26:13 +0100

    with concat

--- concat.ts
+++ concat.ts
@@ -1,7 +1,26 @@
 import { writeCSV } from "jsr:@vslinko/csv";
 
 
-const filenames = ["res1.json"];
+let debug = false;
+
+async function getFilenames() {
+    const filenames = [];
+
+    if (debug) {
+        filenames.push("res1.json");
+    }
+    for await (const dirEntry of Deno.readDir("result")) {
+        const name = dirEntry.name;
+        if (!name.endsWith('.json')) {
+            continue;
+        }
+        filenames.push("result/" + dirEntry.name);
+    }
+
+    return filenames;
+}
+
+
 
 
 const f = await Deno.open("./result.csv", {
@@ -14,7 +33,7 @@ const rows = [
     ["content", "date"],
 ];
 
-for (const file of filenames) {
+for (const file of await getFilenames()) {
     const content = JSON.parse(await Deno.readTextFile(file));
     for (const line of content["reviews"]) {
         rows.push([line.content, line.date]);
